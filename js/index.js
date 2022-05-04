3afda87a3be6db51928fe39838db2b2bab81c3f8
Raihan Morshed <raihan_morshed@hotmail.com>
2022-05-04 11:56:27 -0400

    added data.json file, JS function and the promise that fetches data

--- js/index.js
+++ js/index.js
@@ -2,3 +2,22 @@ document.querySelector(".button-container").addEventListener("click", () => {
     let text = document.getElementById("filter-jobs").value;
     //console.log(text);
 })
+
+function getJobs() {
+    return fetch("data.json").then(response => response.json())
+    .then(data => {
+        console.log(data);
+        return data;
+    })
+}
+
+function showJobs(jobs) {
+    console.log("Jobs in showJobs", jobs);
+}
+
+
+
+
+getJobs().then(data => {
+    showJobs(data);
+});
